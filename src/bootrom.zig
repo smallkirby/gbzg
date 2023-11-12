05a7b5ca65c2d3bad326a7b7f5e0f76cccff25d7
smallkirby <ssmallkirby@gmail.com>
2023-11-12 13:02:07 +0900

    Add bootrom activation feature
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/bootrom.zig
+++ src/bootrom.zig
@@ -1,5 +1,6 @@
 const Bootrom = struct {
     rom: [*]u8,
+    active: bool = true,
 
     pub fn new(rom: [*]u8) Bootrom {
         return Bootrom{ .rom = rom };
@@ -7,6 +8,13 @@ const Bootrom = struct {
 
     pub fn read(self: Bootrom, addr: u16) u8 {
         return self.rom[addr];
+    }
+
+    pub fn write(self: *Bootrom, addr: u16, val: u8) void {
+        if (addr != 0xFF50) {
+            unreachable;
+        }
+        self.active = val == 0x00;
     }
 };
 
@@ -22,4 +30,15 @@ test "bootrom simple read" {
     try expect(bootrom.read(0x03) == 0x03);
     try expect(bootrom.read(0x04) == 0x04);
     try expect(bootrom.read(0x05) == 0x05);
+}
+
+test "bootrom activate" {
+    var rom = [_]u8{ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05 };
+    var bootrom = Bootrom.new(&rom);
+
+    try expect(bootrom.active == true);
+    bootrom.write(0xFF50, 0x01);
+    try expect(bootrom.active == false);
+    bootrom.write(0xFF50, 0x00);
+    try expect(bootrom.active == true);
 }
