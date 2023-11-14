d56513d584d27191e92623995719301f8686c8be
smallkirby <ssmallkirby@gmail.com>
2023-11-14 18:31:35 +0900

    use static variable to save state for instructions
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/cpu/instruction.zig
+++ src/cpu/instruction.zig
@@ -1,4 +1,5 @@
 //! This file defines the instruction set of CPU.
+//! Note that all the instruction consumes at least 1-cycle to fetch the next instruction.
 
 const Cpu = @import("cpu.zig").Cpu;
 const Registers = @import("register.zig").Registers;
@@ -12,24 +13,35 @@ pub fn nop(cpu: *Cpu, bus: *Peripherals) void {
 
 /// Load or move a value from a source to a destination.
 pub fn ld(cpu: *Cpu, bus: *Peripherals, dst: Operand, src: Operand) void {
-    switch (cpu.ctx.inst_ctx.step orelse 0) {
-        0 => {
-            const v = src.read(cpu, bus);
-            if (v != null) {
-                cpu.ctx.inst_ctx.step = 1;
-                cpu.ctx.inst_ctx.cache = v.?;
-            }
-        },
-        1 => {
-            if (dst.write(cpu, bus, cpu.ctx.inst_ctx.cache.?) != null) {
-                cpu.ctx.inst_ctx.step = 2;
-            }
-        },
-        2 => {
-            cpu.ctx.inst_ctx.step = null;
-            cpu.fetch(bus);
-        },
-        else => unreachable,
+    const state = struct {
+        var step: usize = 0;
+        var cache: u16 = 0;
+    };
+    while (true) {
+        switch (state.step) {
+            0 => blk: {
+                const v = src.read(cpu, bus);
+                if (v != null) {
+                    state.step = 1;
+                    state.cache = v.?;
+                    break :blk;
+                }
+                return;
+            },
+            1 => blk: {
+                if (dst.write(cpu, bus, state.cache) != null) {
+                    state.step = 2;
+                    break :blk;
+                }
+                return;
+            },
+            2 => {
+                state.step = 0;
+                cpu.fetch(bus);
+                return;
+            },
+            else => unreachable,
+        }
     }
 }
 
@@ -51,127 +63,150 @@ pub fn cp(cpu: *Cpu, bus: *Peripherals, src: Operand) void {
 
 /// Increment a value of an operand, then set flags according to the result if necessary.
 pub fn inc(cpu: *Cpu, bus: *Peripherals, src: Operand) void {
-    switch (cpu.ctx.inst_ctx.step orelse 0) {
-        0 => {
-            const v = src.read(cpu, bus);
-            if (v != null) {
-                if (src.is8()) {
-                    const u: u8 = @intCast(v.? & 0xFF);
-                    const res = u +% 1;
-                    cpu.regs.set_zf(res == 0);
-                    cpu.regs.set_nf(false); // unconditional
-                    cpu.regs.set_hf((u & 0x0F) == 0x0F);
-                    cpu.ctx.inst_ctx.cache = @intCast(res);
-                } else {
-                    const u: u16 = v.?;
-                    const res = u +% 1;
-                    cpu.ctx.inst_ctx.cache = res;
-                }
+    const state = struct {
+        var step: usize = 0;
+        var cache: u16 = 0;
+    };
+    while (true) {
+        switch (state.step) {
+            0 => blk: {
+                const v = src.read(cpu, bus);
+                if (v != null) {
+                    if (src.is8()) {
+                        const u: u8 = @intCast(v.? & 0xFF);
+                        const res = u +% 1;
+                        cpu.regs.set_zf(res == 0);
+                        cpu.regs.set_nf(false); // unconditional
+                        cpu.regs.set_hf((u & 0x0F) == 0x0F);
+                        state.cache = @intCast(res);
+                    } else {
+                        const u: u16 = v.?;
+                        const res = u +% 1;
+                        state.cache = res;
+                    }
 
-                cpu.ctx.inst_ctx.step = 1;
-            }
-        },
-        1 => {
-            if (src.write(cpu, bus, cpu.ctx.inst_ctx.cache.?) != null) {
-                if (src.is8()) {
-                    cpu.fetch(bus);
-                    cpu.ctx.inst_ctx.step = null;
-                } else {
-                    cpu.ctx.inst_ctx.step = 2;
+                    state.step = 1;
+                    break :blk;
                 }
-            }
-        },
-        2 => {
-            cpu.fetch(bus);
-            cpu.ctx.inst_ctx.step = null;
-        },
-        else => unreachable,
+                return;
+            },
+            1 => {
+                if (src.write(cpu, bus, state.cache) != null) {
+                    if (src.is8()) {
+                        cpu.fetch(bus);
+                        state.step = 0;
+                    } else {
+                        state.step = 2;
+                    }
+                }
+                return;
+            },
+            2 => {
+                state.step = 0;
+                cpu.fetch(bus);
+                return;
+            },
+            else => unreachable,
+        }
     }
 }
 
 /// Decrement a value of an operand, then set flags according to the result if necessary.
 pub fn dec(cpu: *Cpu, bus: *Peripherals, src: Operand) void {
-    switch (cpu.ctx.inst_ctx.step orelse 0) {
-        0 => {
-            const v = src.read(cpu, bus);
-            if (v != null) {
-                if (src.is8()) {
-                    const u: u8 = @intCast(v.? & 0xFF);
-                    const res = u -% 1;
-                    cpu.regs.set_zf(res == 0);
-                    cpu.regs.set_nf(true); // unconditional
-                    cpu.regs.set_hf((u & 0x0F) == 0x00);
-                    cpu.ctx.inst_ctx.cache = @intCast(res);
-                } else {
-                    const u: u16 = v.?;
-                    const res = u -% 1;
-                    cpu.ctx.inst_ctx.cache = res;
-                }
+    const state = struct {
+        var step: usize = 0;
+        var cache: u16 = 0;
+    };
+    while (true) {
+        switch (state.step) {
+            0 => blk: {
+                const v = src.read(cpu, bus);
+                if (v != null) {
+                    if (src.is8()) {
+                        const u: u8 = @intCast(v.? & 0xFF);
+                        const res = u -% 1;
+                        cpu.regs.set_zf(res == 0);
+                        cpu.regs.set_nf(true); // unconditional
+                        cpu.regs.set_hf((u & 0x0F) == 0);
+                        state.cache = @intCast(res);
+                    } else {
+                        const u: u16 = v.?;
+                        const res = u -% 1;
+                        state.cache = res;
+                    }
 
-                cpu.ctx.inst_ctx.step = 1;
-            }
-        },
-        1 => {
-            if (src.write(cpu, bus, cpu.ctx.inst_ctx.cache.?) != null) {
-                if (src.is8()) {
-                    cpu.fetch(bus);
-                    cpu.ctx.inst_ctx.step = null;
-                } else {
-                    cpu.ctx.inst_ctx.step = 2;
+                    state.step = 1;
+                    break :blk;
                 }
-            }
-        },
-        2 => {
-            cpu.fetch(bus);
-            cpu.ctx.inst_ctx.step = null;
-        },
-        else => unreachable,
+                return;
+            },
+            1 => {
+                if (src.write(cpu, bus, state.cache) != null) {
+                    if (src.is8()) {
+                        cpu.fetch(bus);
+                        state.step = 0;
+                    } else {
+                        state.step = 2;
+                    }
+                }
+                return;
+            },
+            2 => {
+                state.step = 0;
+                cpu.fetch(bus);
+                return;
+            },
+            else => unreachable,
+        }
     }
 }
 
 /// Bit shift src and append C-flag to the least significant bit.
 /// Then set flags according to the result if necessary.
 pub fn rl(cpu: *Cpu, bus: *Peripherals, src: Operand) void {
-    switch (cpu.ctx.inst_ctx.step orelse 0) {
-        0 => {
-            const s = src.read(cpu, bus);
-            if (s != null) {
-                const u: u8 = @intCast(s.? & 0xFF);
-                const v = (u << 1) | @intFromBool(cpu.regs.cf());
-                cpu.regs.set_zf(v == 0);
-                cpu.regs.set_nf(false); // unconditional
-                cpu.regs.set_hf(false); // unconditional
-                cpu.regs.set_cf((u & 0x80) != 0);
-                cpu.ctx.inst_ctx.cache = @intCast(v);
-                cpu.ctx.inst_ctx.step = 1;
-            }
-        },
-        1 => {
-            if (src.write(cpu, bus, cpu.ctx.inst_ctx.cache.?) != null) {
-                cpu.fetch(bus);
-                cpu.ctx.inst_ctx.step = null;
-            }
-        },
-        else => unreachable,
+    const state = struct {
+        var step: usize = 0;
+        var cache: u8 = 0;
+    };
+    while (true) {
+        switch (state.step) {
+            0 => blk: {
+                const s = src.read(cpu, bus);
+                if (s != null) {
+                    const u: u8 = @intCast(s.? & 0xFF);
+                    const v = (u << 1) | @intFromBool(cpu.regs.cf());
+                    cpu.regs.set_zf(v == 0);
+                    cpu.regs.set_nf(false); // unconditional
+                    cpu.regs.set_hf(false); // unconditional
+                    cpu.regs.set_cf((u & 0x80) != 0);
+                    state.cache = v;
+                    state.step = 1;
+                    break :blk;
+                }
+                return;
+            },
+            1 => {
+                if (src.write(cpu, bus, @intCast(state.cache)) != null) {
+                    cpu.fetch(bus);
+                    state.step = 0;
+                }
+                return;
+            },
+            else => unreachable,
+        }
     }
 }
 
 /// Check if num-th bit of src is NOT set.
 pub fn bit(cpu: *Cpu, bus: *Peripherals, nth: u3, src: Operand) void {
-    switch (cpu.ctx.inst_ctx.step orelse 0) {
-        0 => {
-            const s = src.read(cpu, bus);
-            if (s != null) {
-                const u: u8 = @intCast(s.? & 0xFF);
-                cpu.regs.set_zf((u & (@as(u8, 1) << nth)) == 0);
-                cpu.regs.set_nf(false); // unconditional
-                cpu.regs.set_hf(true); // unconditional
-                cpu.ctx.inst_ctx.step = null;
+    const s = src.read(cpu, bus);
+    if (s != null) {
+        const u: u8 = @intCast(s.? & 0xFF);
+        cpu.regs.set_zf((u & (@as(u8, 1) << nth)) == 0);
+        cpu.regs.set_nf(false); // unconditional
+        cpu.regs.set_hf(true); // unconditional
 
-                cpu.fetch(bus);
-            }
-        },
-        else => unreachable,
+        cpu.fetch(bus);
     }
 }
 
@@ -194,10 +229,7 @@ test "ld" {
     peripherals.write(0xC000, 0x89);
     ld(&cpu, &peripherals, .{ .reg8 = .A }, .{ .imm8 = .{} });
     ld(&cpu, &peripherals, .{ .reg8 = .A }, .{ .imm8 = .{} });
-    try expect(cpu.regs.a != 0x89);
-    ld(&cpu, &peripherals, .{ .reg8 = .A }, .{ .imm8 = .{} });
     try expect(cpu.regs.a == 0x89);
-    ld(&cpu, &peripherals, .{ .reg8 = .A }, .{ .imm8 = .{} });
     try expect(cpu.regs.pc == 0xC002);
 
     // d=Direct8, s=Reg8, 4-cycle, 3-PC
@@ -205,12 +237,10 @@ test "ld" {
     cpu.regs.a = 0x23;
     peripherals.write(cpu.regs.pc, 0x20);
     peripherals.write(cpu.regs.pc + 1, 0xC0);
-    for (0..5) |_| {
+    for (0..4) |_| {
         ld(&cpu, &peripherals, .{ .direct8 = .D }, .{ .reg8 = .A });
     }
     try expect(peripherals.read(0xC020) == 0x23);
-    try expect(cpu.regs.pc == 0xC002);
-    ld(&cpu, &peripherals, .{ .direct8 = .D }, .{ .reg8 = .A });
     try expect(cpu.regs.pc == 0xC003);
 
     // d=Reg16, s=Imm16, 3-cycle, 3-PC
@@ -218,12 +248,10 @@ test "ld" {
     cpu.regs.pc = 0xC000;
     peripherals.write(cpu.regs.pc, 0x34);
     peripherals.write(cpu.regs.pc + 1, 0x12);
-    for (0..4) |_| {
+    for (0..3) |_| {
         ld(&cpu, &peripherals, .{ .reg16 = .BC }, .{ .imm16 = .{} });
     }
     try expect(cpu.regs.bc() == 0x1234);
-    try expect(cpu.regs.pc == 0xC002);
-    ld(&cpu, &peripherals, .{ .reg16 = .BC }, .{ .imm16 = .{} });
     try expect(cpu.regs.pc == 0xC003);
 }
 
@@ -280,8 +308,9 @@ test "cp" {
     cpu.regs.pc = 0xC000;
     cpu.regs.write_bc(0xC000);
     peripherals.write(cpu.regs.bc(), 0x12);
-    cp(&cpu, &peripherals, .{ .indirect = .BC });
-    cp(&cpu, &peripherals, .{ .indirect = .BC });
+    for (0..2) |_| {
+        cp(&cpu, &peripherals, .{ .indirect = .BC });
+    }
     try expect(cpu.regs.zf() == true);
     try expect(cpu.regs.nf() == true);
     try expect(cpu.regs.hf() == false);
@@ -297,7 +326,6 @@ test "inc" {
     cpu.regs.pc = 0xC000;
     cpu.regs.b = 0x12;
     inc(&cpu, &peripherals, .{ .reg8 = .B });
-    inc(&cpu, &peripherals, .{ .reg8 = .B });
     try expect(cpu.regs.b == 0x13);
     try expect(cpu.regs.zf() == false);
     try expect(cpu.regs.nf() == false);
@@ -306,7 +334,6 @@ test "inc" {
     try expect(cpu.regs.pc == 0xC001);
 
     cpu.regs.b = 0xFF;
-    inc(&cpu, &peripherals, .{ .reg8 = .B });
     inc(&cpu, &peripherals, .{ .reg8 = .B });
     try expect(cpu.regs.b == 0x00);
     try expect(cpu.regs.zf() == true);
@@ -327,8 +354,6 @@ test "inc" {
     try expect(cpu.regs.nf() == false);
     try expect(cpu.regs.hf() == false);
     try expect(cpu.regs.cf() == false);
-    try expect(cpu.regs.pc == 0xC000);
-    inc(&cpu, &peripherals, .{ .indirect = .BC });
     try expect(cpu.regs.pc == 0xC001);
 
     // src=Reg16, 2-cycle, 1-PC
@@ -337,8 +362,6 @@ test "inc" {
     inc(&cpu, &peripherals, .{ .reg16 = .BC });
     inc(&cpu, &peripherals, .{ .reg16 = .BC });
     try expect(cpu.regs.bc() == 0x1235);
-    try expect(cpu.regs.pc == 0xC000);
-    inc(&cpu, &peripherals, .{ .reg16 = .BC });
     try expect(cpu.regs.pc == 0xC001);
 }
 
@@ -350,7 +373,6 @@ test "dec" {
     cpu.regs.pc = 0xC000;
     cpu.regs.b = 0x12;
     dec(&cpu, &peripherals, .{ .reg8 = .B });
-    dec(&cpu, &peripherals, .{ .reg8 = .B });
     try expect(cpu.regs.b == 0x11);
     try expect(cpu.regs.zf() == false);
     try expect(cpu.regs.nf() == true);
@@ -359,7 +381,6 @@ test "dec" {
     try expect(cpu.regs.pc == 0xC001);
 
     cpu.regs.b = 0x01;
-    dec(&cpu, &peripherals, .{ .reg8 = .B });
     dec(&cpu, &peripherals, .{ .reg8 = .B });
     try expect(cpu.regs.b == 0x00);
     try expect(cpu.regs.zf() == true);
@@ -380,8 +401,6 @@ test "dec" {
     try expect(cpu.regs.nf() == true);
     try expect(cpu.regs.hf() == true);
     try expect(cpu.regs.cf() == false);
-    try expect(cpu.regs.pc == 0xC000);
-    dec(&cpu, &peripherals, .{ .indirect = .BC });
     try expect(cpu.regs.pc == 0xC001);
 }
 
@@ -389,11 +408,11 @@ test "rl" {
     var cpu = Cpu.new();
     var peripherals = try tutil.t_init_peripherals();
 
-    // src=Reg8, 1-cycle
+    // src=Reg8, 1-cycle (+1 for decode)
     cpu.regs.pc = 0xC000;
     cpu.regs.a = 0x12;
     cpu.regs.set_cf(true);
-    for (0..2) |_| {
+    for (0..1) |_| {
         rl(&cpu, &peripherals, .{ .reg8 = .A });
     }
     try expect(cpu.regs.a == 0x25);
@@ -403,12 +422,12 @@ test "rl" {
     try expect(cpu.regs.cf() == false);
     try expect(cpu.regs.pc == 0xC001);
 
-    // src=Indirect, 3-cycle
+    // src=Indirect, 3-cycle (+1 for decode)
     cpu.regs.pc = 0xC000;
     cpu.regs.write_bc(0xC000);
     peripherals.write(cpu.regs.bc(), 0x80);
     cpu.regs.set_cf(false);
-    for (0..4) |_| {
+    for (0..3) |_| {
         rl(&cpu, &peripherals, .{ .indirect = .BC });
     }
     try expect(peripherals.read(cpu.regs.bc()) == 0x00);
@@ -423,7 +442,7 @@ test "bit" {
     var cpu = Cpu.new();
     var peripherals = try tutil.t_init_peripherals();
 
-    // src=Reg8, 2-cycle
+    // src=Reg8, 2-cycle (+1 for decode)
     cpu.regs.pc = 0xC000;
     cpu.regs.a = 0x12;
     for (0..1) |_| {
@@ -435,7 +454,7 @@ test "bit" {
     try expect(cpu.regs.cf() == false);
     try expect(cpu.regs.pc == 0xC001);
 
-    // src=Indirect, 3
+    // src=Indirect, 2-cycle (+1 for decode)
     cpu.regs.pc = 0xC000;
     cpu.regs.write_bc(0xC000);
     peripherals.write(cpu.regs.bc(), 0x40);
