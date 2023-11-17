a174a54c9067d117f50b86a56351b360f08f8caf
smallkirby <ssmallkirby@gmail.com>
2023-11-17 22:07:45 +0900

    mbc: impo write
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/mbc.zig
+++ src/mbc.zig
@@ -5,7 +5,7 @@ pub const Mbc = union(enum) {
     nombc: struct {},
     mbc1: Mbc1,
 
-    /// Instantiate MBC1.
+    /// Instantiate a memory bank controller.
     pub fn new(cartridge_type: CardridgeType, num_rom_banks: usize) @This() {
         return switch (cartridge_type) {
             .MBC1, .MBC1_SRAM, .MBC1_SRAM_BATT => .{
@@ -23,6 +23,24 @@ pub const Mbc = union(enum) {
             },
             _ => unreachable,
         };
+    }
+
+    /// Write to MBC is handled as register writes to control the MBC.
+    pub fn write(self: *@This(), addr: u16, val: u8) void {
+        switch (self.*) {
+            .nombc => {},
+            .mbc1 => |*mbc| switch (addr) {
+                0x0000...0x1FFF => mbc.sram_enabled = (val & 0x0F) == 0x0A,
+                0x2000...0x3FFF => mbc.low_bank = if (val & 0b1111 == 0b0000) b: {
+                    break :b 0b0001; // write 1 instead if val is zero
+                } else b: {
+                    break :b val & 0b0001_1111;
+                },
+                0x4000...0x5FFF => mbc.high_bank = val & 0b11,
+                0x6000...0x7FFF => mbc.bank_mode = val & 0b1 == 0b1,
+                else => unreachable,
+            },
+        }
     }
 };
 
@@ -49,6 +67,44 @@ test "MBC init" {
     try expect(mbc1.high_bank == 0);
     try expect(mbc1.bank_mode == false);
     try expect(mbc1.num_rom_banks == 2);
+}
+
+test "MBC1 write" {
+    var mbc = Mbc.new(.MBC1, 2);
+
+    // SRAM enable
+    try expect(mbc.mbc1.sram_enabled == false);
+    mbc.write(0x0100, 0x0A);
+    try expect(mbc.mbc1.sram_enabled == true);
+    mbc.write(0x0100, 0x3D);
+    try expect(mbc.mbc1.sram_enabled == false);
+    mbc.write(0x0100, 0x2A);
+    try expect(mbc.mbc1.sram_enabled == true);
+
+    // low bank
+    try expect(mbc.mbc1.low_bank == 1);
+    mbc.write(0x2100, 0x00);
+    try expect(mbc.mbc1.low_bank == 1);
+    mbc.write(0x3FFF, 0b0011_1111);
+    try expect(mbc.mbc1.low_bank == 0b0001_1111);
+    mbc.write(0x2100, 0x00);
+    try expect(mbc.mbc1.low_bank == 0b1);
+
+    // high bank
+    try expect(mbc.mbc1.high_bank == 0);
+    mbc.write(0x4100, 0b0110_0111);
+    try expect(mbc.mbc1.high_bank == 0b11);
+    mbc.write(0x4100, 0b0000_0000);
+    try expect(mbc.mbc1.high_bank == 0b00);
+
+    // bank mode
+    try expect(mbc.mbc1.bank_mode == false);
+    mbc.write(0x6100, 0b0000_0001);
+    try expect(mbc.mbc1.bank_mode == true);
+    mbc.write(0x6100, 0b0000_0000);
+    try expect(mbc.mbc1.bank_mode == false);
+    mbc.write(0x6100, 0b1111_1110);
+    try expect(mbc.mbc1.bank_mode == false);
 }
 
 const expect = @import("std").testing.expect;
