27a377ad762fc54cd01df21a1ddfe867b8a54a80
smallkirby <ssmallkirby@gmail.com>
2023-11-19 00:45:27 +0900

    inst: impl rla
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/cpu/instruction.zig
+++ src/cpu/instruction.zig
@@ -623,6 +623,19 @@ pub fn rlca(cpu: *Cpu, bus: *Peripherals) void {
     cpu.fetch(bus);
 }
 
+/// Rotate A-register left through C-flag, then set flags according to the result.
+pub fn rla(cpu: *Cpu, bus: *Peripherals) void {
+    const c = @as(u8, @intFromBool(cpu.regs.cf()));
+    const res = (cpu.regs.a << 1) | c;
+    cpu.regs.set_zf(false); // unconditional
+    cpu.regs.set_nf(false); // unconditional
+    cpu.regs.set_hf(false); // unconditional
+    cpu.regs.set_cf((cpu.regs.a & 0x80) != 0);
+    cpu.regs.a = res;
+
+    cpu.fetch(bus);
+}
+
 test "nop" {
     var cpu = Cpu.new();
     var peripherals = try tutil.t_init_peripherals();
@@ -1546,6 +1559,37 @@ test "rlca" {
     cpu.regs.a = 0b0000_0001;
     for (0..1) |_| {
         rlca(&cpu, &peripherals);
+    }
+    try expect(cpu.regs.a == 0b0000_0010);
+    try expect(cpu.regs.zf() == false);
+    try expect(cpu.regs.nf() == false);
+    try expect(cpu.regs.hf() == false);
+    try expect(cpu.regs.cf() == false);
+}
+
+test "rla" {
+    var cpu = Cpu.new();
+    var peripherals = try tutil.t_init_peripherals();
+
+    // 1-cycle
+    cpu.regs.pc = 0xC000;
+    cpu.regs.a = 0b1000_0001;
+    cpu.regs.set_cf(true);
+    for (0..1) |_| {
+        rla(&cpu, &peripherals);
+    }
+    try expect(cpu.regs.a == 0b0000_0011);
+    try expect(cpu.regs.zf() == false);
+    try expect(cpu.regs.nf() == false);
+    try expect(cpu.regs.hf() == false);
+    try expect(cpu.regs.cf() == true);
+    try expect(cpu.regs.pc == 0xC001);
+
+    cpu.regs.pc = 0xC000;
+    cpu.regs.a = 0b0000_0001;
+    cpu.regs.set_cf(false);
+    for (0..1) |_| {
+        rla(&cpu, &peripherals);
     }
     try expect(cpu.regs.a == 0b0000_0010);
     try expect(cpu.regs.zf() == false);
