1c4a81570251a7c34c2ed84966c9dac8c08a3a1b
smallkirby <ssmallkirby@gmail.com>
2023-11-23 21:17:17 +0900

    joy: change internal repr
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/joypad.zig
+++ src/joypad.zig
@@ -3,92 +3,42 @@ const Interrupts = @import("interrupts.zig").Interrupts;
 const IEB = Interrupts.InterruptsEnableBits;
 
 pub const Joypad = struct {
-    register: JoyRegister,
-
-    /// Joypad register
-    const JoyRegister = packed struct {
-        /// Set to 0 on A button pressed in ActionButton mode, or Right button in DirectionButton mode
-        a: u1,
-        /// Set to 0 on B button pressed in ActionButton mode, or Left button in DirectionButton mode
-        b: u1,
-        /// Set to 0 on Select button pressed in ActionButton mode, or Up button in DirectionButton mode
-        select: u1,
-        /// Set to 0 on Start button pressed in ActionButton mode, or Down button in DirectionButton mode
-        start: u1,
-        /// If set, DirectionButton mode is enabled.
-        direction_enabled: u1,
-        /// If set, ActionButton mode is enabled.
-        action_enabled: u1,
-        /// Unused (always 1)
-        _unused: u2 = 0b11,
-
-        fn val(self: @This()) u8 {
-            return @as(u8, @bitCast(self));
-        }
-
-        pub fn new() @This() {
-            return @This(){
-                .a = 1,
-                .b = 1,
-                .select = 1,
-                .start = 1,
-                .direction_enabled = 1,
-                .action_enabled = 1,
-            };
-        }
-
-        pub fn set(self: *@This(), button: Button) void {
-            switch (button) {
-                .Down => self.a = 1,
-                .Up => self.b = 1,
-                .Left => self.select = 1,
-                .Right => self.start = 1,
-                .Start => self.start = 1,
-                .Select => self.select = 1,
-                .A => self.a = 1,
-                .B => self.b = 1,
-            }
-        }
-
-        pub fn unset(self: *@This(), button: Button) void {
-            switch (button) {
-                .Down => self.a = 0,
-                .Up => self.b = 0,
-                .Left => self.select = 0,
-                .Right => self.start = 0,
-                .Start => self.start = 0,
-                .Select => self.select = 0,
-                .A => self.a = 0,
-                .B => self.b = 0,
-            }
-        }
-    };
+    mode: u8 = 0,
+    action: u8 = 0xFF,
+    direction: u8 = 0xFF,
 
     pub fn new() @This() {
-        return @This(){
-            .register = JoyRegister.new(),
-        };
+        return @This(){};
     }
 
     pub fn read(self: @This()) u8 {
-        return self.register.val();
+        var ret: u8 = 0b1100_1111 | self.mode;
+        if (self.mode & 0b0001_0000 != 0) {
+            ret &= self.direction;
+        }
+        if (self.mode & 0b0010_0000 != 0) {
+            ret &= self.action;
+        }
+        return ret;
     }
 
     pub fn write(self: *@This(), val: u8) void {
-        self.register = val & 0b0011_0000;
+        self.mode = val & 0b0011_0000;
     }
 
     pub fn button_pressed(self: *@This(), intrs: *Interrupts, button: Button) void {
-        self.register.unset(button);
+        self.direction &= ~button.as_direction();
+        self.action &= ~button.as_action();
         intrs.irq(@intFromEnum(IEB.JOYPAD));
     }
 
     pub fn button_released(self: *@This(), button: Button) void {
-        self.register.set(button);
+        self.direction |= button.as_direction();
+        self.action |= button.as_action();
     }
 };
 
-pub const Button = enum {
+pub const Button = enum(u8) {
     Down,
     Up,
     Left,
@@ -97,33 +47,39 @@ pub const Button = enum {
     Select,
     A,
     B,
+
+    pub fn as_direction(self: @This()) u8 {
+        return switch (self) {
+            .Down => 0b1000,
+            .Up => 0b0100,
+            .Left => 0b0010,
+            .Right => 0b0001,
+            else => 0,
+        };
+    }
+
+    pub fn as_action(self: @This()) u8 {
+        return switch (self) {
+            .Start => 0b1000,
+            .Select => 0b0100,
+            .B => 0b0010,
+            .A => 0b0001,
+            else => 0,
+        };
+    }
 };
-
-test "struct JoyRegister" {
-    try expect(@sizeOf(Joypad.JoyRegister) == 1);
-
-    const val = 0b1110_1101;
-    const reg: Joypad.JoyRegister = @bitCast(@as(u8, val));
-
-    try expect(reg.a == 1);
-    try expect(reg.b == 0);
-    try expect(reg.select == 1);
-    try expect(reg.start == 1);
-    try expect(reg.direction_enabled == 0);
-    try expect(reg.action_enabled == 1);
-    try expect(reg._unused == 0b11);
-}
 
 test "button press" {
     var joypad = Joypad.new();
     var intrs = Interrupts.new();
 
+    joypad.write(0b0010_0000);
     joypad.button_pressed(&intrs, Button.A);
     joypad.button_pressed(&intrs, Button.B);
-    try expect(joypad.read() == 0b1111_1100);
+    try expect(joypad.read() == 0b1110_1100);
 
     joypad.button_released(Button.A);
-    try expect(joypad.read() == 0b1111_1101);
+    try expect(joypad.read() == 0b1110_1101);
 }
 
 const expect = std.testing.expect;
